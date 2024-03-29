389a407a901ba10eef143981f8af97d8a799b5c6
Thiago Hersan <thiago.hersan+github@gmail.com>
2024-03-28 20:41:47 -0400

    4: some valid neighbor-aware collapsing

---
+++ 01-wave/4/sketch.js
@@ -0,0 +1,88 @@
+const GRID_SIZE = 200;
+
+const grid = [];
+
+function toI(x, y) {
+  const x_count = ceil(width / GRID_SIZE);
+  const x_p = floor(x / GRID_SIZE);
+  const y_p = floor(y / GRID_SIZE);
+  return y_p * x_count + x_p;
+}
+
+function updateGridElement(aGridy) {
+  const mX = aGridy.x;
+  const mY = aGridy.y;
+
+  for (let p = 0; p < aGridy.possibilities.length; p++) {
+    const tileIdx = aGridy.possibilities[p];
+
+    // update RIGHT
+    if (mX + GRID_SIZE < width) {
+      grid[toI(mX + GRID_SIZE, mY)].updateFromLeft(tileIdx);
+    }
+
+    // update BOTTOM
+    if (mY + GRID_SIZE < height) {
+      grid[toI(mX, mY + GRID_SIZE)].updateFromTop(tileIdx);
+    }
+
+    // update LEFT
+    if (mX > GRID_SIZE) {
+      grid[toI(mX - GRID_SIZE, mY)].updateFromRight(tileIdx);
+    }
+
+    // update TOP
+    if (mY > GRID_SIZE) {
+      grid[toI(mX, mY - GRID_SIZE)].updateFromBottom(tileIdx);
+    }
+  }
+}
+
+function collapse(aGridy) {
+  aGridy.possibilities = [random(aGridy.possibilities)];
+  updateGridElement(aGridy);
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  noLoop();
+  noStroke();
+
+  for (let y = 0; y < height; y += GRID_SIZE) {
+    for (let x = 0; x < width; x += GRID_SIZE) {
+      grid.push(new Gridy(x, y));
+    }
+  }
+}
+
+function draw() {
+  background(250, 50, 150);
+
+  const byPossibilities = grid.toSorted((a, b) => a.possibilities.length - b.possibilities.length);
+  const candidates = byPossibilities.filter((p) => p.possibilities.length > 1);
+
+  if (candidates.length > 0) {
+    const candidate = candidates[0];
+    print("collapsing", toI(candidate.x, candidate.y));
+    collapse(grid[toI(candidate.x, candidate.y)]);
+  } else {
+    noLoop();
+  }
+
+  for (let i = 0; i < grid.length; i++) {
+    const mGridy = grid[i];
+    let mTile = TILES[mGridy.possibilities[0]];
+
+    // DEBUG
+    try {
+      mTile.draw(mGridy.x, mGridy.y, GRID_SIZE);
+    } catch {
+      print(i, mTile);
+    }
+    text(i + ": " + mGridy.possibilities, mGridy.x, mGridy.y, GRID_SIZE, GRID_SIZE);
+  }
+}
+
+function mouseClicked() {
+  redraw();
+}
