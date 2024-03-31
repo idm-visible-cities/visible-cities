212bd636475d1a98de74fd3ce96b7b2d406827f7
Thiago Hersan <thiago.hersan+github@gmail.com>
2024-03-30 21:30:55 -0400

    DIY set union function

--- 01-wave/5/Gridy.js
+++ 01-wave/5/Gridy.js
@@ -6,6 +6,13 @@ class Gridy {
     this.possibilities = ALL_TILE_IDXS.slice();
   }
 
+  static setUnion(s0, s1) {
+    const newSet = new Set();
+    s0.forEach(v => newSet.add(v));
+    s1.forEach(v => newSet.add(v));
+    return newSet;
+  }
+
   debug(w, h = 0) {
     h = h || w;
     const t = `${this.xi}x${this.yi} (${this.i}): ${this.possibilities}`;
@@ -16,7 +23,7 @@ class Gridy {
     let newPossibilities = new Set([]);
     for (let ti = 0; ti < tileIdxs.length; ti++) {
       const allowed = this.possibilities.filter((p) => TILES[p].allowRight(tileIdxs[ti]));
-      newPossibilities = newPossibilities.union(new Set(allowed));
+      newPossibilities = Gridy.setUnion(newPossibilities, new Set(allowed));
     }
     this.possibilities = Array.from(newPossibilities);
   }
@@ -25,7 +32,7 @@ class Gridy {
     let newPossibilities = new Set([]);
     for (let ti = 0; ti < tileIdxs.length; ti++) {
       const allowed = this.possibilities.filter((p) => TILES[p].allowBottom(tileIdxs[ti]));
-      newPossibilities = newPossibilities.union(new Set(allowed));
+      newPossibilities = Gridy.setUnion(newPossibilities, new Set(allowed));
     }
     this.possibilities = Array.from(newPossibilities);
   }
@@ -34,7 +41,7 @@ class Gridy {
     let newPossibilities = new Set([]);
     for (let ti = 0; ti < tileIdxs.length; ti++) {
       const allowed = this.possibilities.filter((p) => TILES[p].allowLeft(tileIdxs[ti]));
-      newPossibilities = newPossibilities.union(new Set(allowed));
+      newPossibilities = Gridy.setUnion(newPossibilities, new Set(allowed));
     }
     this.possibilities = Array.from(newPossibilities);
   }
@@ -43,7 +50,7 @@ class Gridy {
     let newPossibilities = new Set([]);
     for (let ti = 0; ti < tileIdxs.length; ti++) {
       const allowed = this.possibilities.filter((p) => TILES[p].allowTop(tileIdxs[ti]));
-      newPossibilities = newPossibilities.union(new Set(allowed));
+      newPossibilities = Gridy.setUnion(newPossibilities, new Set(allowed));
     }
     this.possibilities = Array.from(newPossibilities);
   }
